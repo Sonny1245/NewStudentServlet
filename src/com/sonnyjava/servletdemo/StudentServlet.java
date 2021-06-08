b6e6bc4849807a3578f9d871a9d6cd18d0438c62
Son <son@ISD-EDLMAC-L01.local>
2021-06-08 14:40:33 -0700

    first commit

---
+++ src/com/sonnyjava/servletdemo/StudentServlet.java
@@ -0,0 +1,59 @@
+package com.sonnyjava.servletdemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		 //Step 1: setContentType
+		
+		response.setContentType("text/html");
+		
+		//Step 2: printWriter
+		
+		PrintWriter out = response.getWriter();
+		
+		//Step 3: Generate the HTML
+		
+		out.println("<html><body>");
+		
+		out.println("Student Infor is confirmed: "
+				+ request.getParameter("firstName")
+				+ " " + request.getParameter("lastName"));
+		out.println("</body></html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
